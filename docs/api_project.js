b82b12df63840cdeb200135775e03aa94fe9fa66
Eric <eric_gomez_1@hotmail.com>
2019-05-11 11:56:35 -0700

    moved the docs to a different folder

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-11T18:47:48.144Z",
+    "time": "2019-05-11T18:56:11.846Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
