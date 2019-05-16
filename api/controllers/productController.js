2551b1ecf25eb8a0001dd33e0a8be31f42dd916f
Eric <eric_gomez_1@hotmail.com>
2019-05-15 22:55:49 -0700

    updated docs, added documentation annotations to the product controller. closes #12

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,5 +1,33 @@
 const service = require('../services/productService');
 
+/**
+ * @api {get} /api/product Request All Products 
+ * @apiVersion 1.0.0
+ * @apiName GETProducts
+ * @apiGroup Product
+ *
+ * @apiSuccess {object[]} List product information array.
+ * @apiParam {query} Category returns all products in the category
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *      [
+ *        {
+ *          "id": 1,
+ *          "first_name": "john",
+ *          "last_name": "doe",
+ *          "role_id": 2
+*         },
+ *      ]
+ *
+ * @apiError ServerError server couldnt process the request
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Server Error
+ *     {
+ *          "error": "server was unable to process the request."
+ *     }
+ */
 const findAllProducts = (req, res) => {
     const { category } = req.query;
     if (!category) {
@@ -12,6 +40,33 @@ const findAllProducts = (req, res) => {
         .catch(err => res.status(500).json(err));
     }
 }
+
+/**
+ * @api {get} /api/product/:id Request Product By Id
+ * @apiVersion 1.0.0
+ * @apiName GETProductById
+ * @apiGroup Product
+ *
+ * @apiSuccess {object[]} List product information array.
+ * @apiParam {number} ID returns a product by id.
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *        {
+ *          "id": 1,
+ *          "first_name": "john",
+ *          "last_name": "doe",
+ *          "role_id": 2
+ *        }
+ *
+ * @apiError ServerError server couldnt process the request
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Server Error
+ *     {
+ *          "error": "server was unable to process the request."
+ *     }
+ */
 
 const findProductById = (req, res) => {
     const { id } = req.params;
