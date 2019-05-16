2ce2fece48469bd77e6b69a6934341b617b11093
Eric <eric_gomez_1@hotmail.com>
2019-05-15 17:51:58 -0700

    implemented complete CRUD service functions for the product table

--- api/services/productService.js
+++ api/services/productService.js
@@ -2,10 +2,18 @@ const knex = require('../../db/knex');
 
 const findAllProducts = () => knex('product').select();
 
-const findProductById = (id) => knex('product').select().where({id}).first();
+const findProductById = (id) => knex('product').select().where({ id }).first();
 
 const findProductByCategory = (category) => knex
                                             .from('product')
                                             .innerJoin('category', 'product.category_id', 'category.id')
                                             .where({'category.description': category});
 
+const saveProduct = (product) => knex('product').insert(product);
+
+//accepts the id of the table to be updated, and the fields to be updated in an object.
+const updateProduct = (id, product) => knex('product').where({ id }).update(product);
+
+const deleteProduct = (product) => knex('product').where({ id: product.id }).del();
+
+const deleteProductById = (id) => knex('product').where({id}).del();
