b0810351cf9f4688e51d921407e2aaf6ac15f15f
Eric <eric_gomez_1@hotmail.com>
2019-05-19 16:58:40 -0700

    implemented CRUD methods for the review, comment and helpful tables. closes #18

--- api/services/reviewService.js
+++ api/services/reviewService.js
@@ -1,2 +1,37 @@
 const knex = require('../../db/knex');
 
+const findAllReviews = () => knex('review').select();
+
+const findReviewById = (id) => knex('review').where({id});
+
+const findAllCommentsByReviewId = (review_id) => knex('comment').where({review_id});
+
+const findCommentById = (id) => knex('comment').where({id});
+
+const findAllHelpfulByReviewId = (review_id) => knex('helpful').where({review_id});
+
+const saveReview = (review) => knex('review').insert(review);
+
+const saveComment = (comment) => knex('comment').insert(comment);
+
+const saveHelpful = (helpful) => knex('helpful').insert(helpful);
+
+const deleteReviewById = (id) => knex('review').where({id}).del();
+
+const deleteCommentById = (id) => knex('comment').where({id}).del();
+
+const deleteHelpful = (helpful) => knex('helpful').where({helpful}).del();
+
+module.exports = {
+    findAllReviews,
+    findReviewById,
+    findAllCommentsByReviewId,
+    findCommentById,
+    findAllHelpfulByReviewId,
+    saveReview,
+    saveComment,
+    saveHelpful,
+    deleteReviewById,
+    deleteCommentById,
+    deleteHelpful
+}
