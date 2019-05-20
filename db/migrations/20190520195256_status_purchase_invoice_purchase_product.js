132792487fcd8c4805c3ce539e2e723821f9806b
ktan114 <ktaneight@gmail.com>
2019-05-20 19:54:07 -0400

    Completed the status_purchase_invoice_purchase_product schema

---
+++ db/migrations/20190520195256_status_purchase_invoice_purchase_product.js
@@ -0,0 +1,74 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('status', table => {
+      table.increments('id');
+      table.string('title').notNullable();
+    })
+    .createTable('purchase', table => {
+      table.increments('id');
+      table.datetime().defaultTo(knex.fn.now());
+      table
+        .integer('status_id')
+        .references('id')
+        .inTable('status')
+        .onDelete('CASCADE');
+      table
+        .integer('user_id')
+        .references('id')
+        .inTable('user')
+        .onDelete('CASCADE');
+      table
+        .integer('invoice_id')
+        .references('id')
+        .inTable('invoice')
+        .onDelete('CASCADE');
+    })
+    .alterTable('purchase', table => {
+      table.unique(['user_id', 'invoice_id']);
+    })
+    .createTable('invoice', table => {
+      table.increments('id');
+      table.decimal('sub_total');
+      table.decimal('tax');
+      table.timestamp('date_created');
+      table.decimal('total');
+      table.string('shipping_address');
+      table
+        .integer('purchase_id')
+        .references('id')
+        .inTable('purchase')
+        .onDelete('CASCADE');
+      table
+        .integer('payment_id')
+        .references('id')
+        .inTable('payment_id')
+        .onDelete('CASCADE');
+    })
+    .alterTable('invoice', table => {
+      table.unique(['purchase_id', 'payment_id']);
+    })
+    .createTable('purchase_product', table => {
+      table
+        .increments('purchase_id')
+        .references('id')
+        .inTable('purchase')
+        .onDelete('CASCADE');
+      table
+        .increments('product_id')
+        .references('id')
+        .inTable('product')
+        .onDelete('CASCADE');
+      table.integer('quantity');
+    })
+    .alterTable('purchase_product', table => {
+      table.primary(['purchase_id', 'product_id']);
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable('invoice')
+    .dropTable('purchase_product')
+    .dropTable('product')
+    .dropTable('status');
+};
