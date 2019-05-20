caefe53de71a6763f4564132e0ba44f0e054c310
Eric <eric_gomez_1@hotmail.com>
2019-05-19 17:23:29 -0700

    implemented basic crud on the review presentational layer. #19

---
+++ api/controllers/reviewController.js
@@ -0,0 +1,33 @@
+const service = require('../services/reviewService');
+
+const findAllReviews = (req, res) => 
+    service.findAllReviews()
+        .then(reviews => res.json(reviews))
+        .catch(err => res.status(500).json(err));
+
+const findReviewById = (req, res) => {
+    const { id } = req.params;
+
+    return service.findReviewById(id)
+        .then(review => res.json(review))
+        .catch(err => res.status(500).json(err));
+}
+
+const saveReview = (req,res) => {
+    const { review } = req.body;
+
+    service.saveReview(review)
+        .then(() =>{
+            service.findAllReviews()
+        })
+        .then(reviews => res.json(reviews))
+        .catch(err => res.status(400).json(err));
+}
+
+const deleteReview = (req, res) => {
+    const { id } = req.params;
+
+    service.deleteReviewById(id)
+        .then(() => res.status(204).json({}))
+        .catch(err => res.status(400).json(err));
+}
