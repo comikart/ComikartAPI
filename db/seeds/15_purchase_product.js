3ed1bcf3a854f8cd99ed0f41b3db4f163a85da4d
ktan114 <ktaneight@gmail.com>
2019-05-23 19:01:36 -0400

    Created the purchase_product seed closes #51

---
+++ db/seeds/15_purchase_product.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('table_name')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        { purchase_id: 1, product_id: 2, quantity: 1 },
+        { purchase_id: 1, product_id: 1, quantity: 3 },
+        { purchase_id: 1, product_id: 5, quantity: 2 },
+        { purchase_id: 1, product_id: 3, quantity: 3 },
+        { purchase_id: 2, product_id: 1, quantity: 2 },
+        { purchase_id: 2, product_id: 5, quantity: 5 },
+        { purchase_id: 3, product_id: 2, quantity: 8 },
+        { purchase_id: 3, product_id: 4, quantity: 2 },
+        { purchase_id: 3, product_id: 5, quantity: 3 },
+      ]);
+    });
+};
