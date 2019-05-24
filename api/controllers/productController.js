388205e83b65c2d0ac265c7d326930faefc4209c
Eric <eric_gomez_1@hotmail.com>
2019-05-24 12:00:55 -0700

    refactored the findAllProducts function, and implemented pagination. closes #61

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,7 +1,7 @@
 const service = require('../services/productService');
 
 /**
- * @api {get} /api/product Request All Products 
+ * @api {get} /api/product Request All Products
  * @apiVersion 1.0.0
  * @apiName GETProducts
  * @apiGroup Product
@@ -17,21 +17,18 @@ const service = require('../services/productService');
  *          "first_name": "john",
  *          "last_name": "doe",
  *          "role_id": 2
-*         },
+ *         },
  *      ]
  */
 const findAllProducts = (req, res) => {
-    const { category } = req.query;
-    if (!category) {
-        return service.findAllProducts()
-        .then(products => res.json(products))
-        .catch(err => res.status(500).json(err));
-    } else {
-        return service.findProductByCategory(category)
-        .then(products => res.json(products))
-        .catch(err => res.status(500).json(err));
-    }
-}
+  const { category, page = 1, count = 10 } = req.query;
+  (!category
+    ? service.findAllProducts(page, count)
+    : service.findProductByCategory(category, page, count)
+  )
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json(err));
+};
 
 /**
  * @api {get} /api/product/:id Request Product By Id
@@ -41,7 +38,7 @@ const findAllProducts = (req, res) => {
  *
  * @apiSuccess {object[]} List product information array.
  * @apiParam {Param} ID returns a product by id.
- * 
+ *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *        {
@@ -53,14 +50,15 @@ const findAllProducts = (req, res) => {
  */
 
 const findProductById = (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    return service.findProductById(id)
+  return service
+    .findProductById(id)
     .then(product => res.json(product))
     .catch(err => res.status(400).json(err));
-}
+};
 
 module.exports = {
-    findAllProducts,
-    findProductById,
-}
+  findAllProducts,
+  findProductById
+};
