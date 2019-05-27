c3eff9be12e3cac9a478d2df8186a3b3e52ea40d
ktan114 <ktaneight@gmail.com>
2019-05-27 19:08:24 -0400

    Remove part of service because it's unnecessary

--- api/services/paymentTypeService.js
+++ api/services/paymentTypeService.js
@@ -6,27 +6,6 @@ const findAllPaymentType = () => knex('payment_type').select();
 // GET by Id
 const findPaymentTypeById = id => knex('payment_type').where({ id });
 
-// POST
-const savePaymentType = paymentType => knex('payment_type').insert(paymentType);
-
-// PUT
-const updatePaymentType = (id, paymentType) =>
-  knex('payment_type')
-    .where({ id })
-    .update(paymentType);
-
-// DELETE
-const deletePaymentType = paymentType =>
-  knex('payment_type')
-    .where({ id: paymentType.id })
-    .del();
-
-// DELETE by Id
-const deletePaymentTypeById = id =>
-  knex('payment_type')
-    .where({ id })
-    .del();
-
 module.exports = {
   findAllPaymentType,
   findPaymentTypeById,
