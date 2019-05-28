587275c895ea88768f7aff4e9a2d498271bb2dbe
ktan114 <ktaneight@gmail.com>
2019-05-28 18:46:31 -0400

    Added additional test cases

--- api/controllers/paymentOptionController.spec.js
+++ api/controllers/paymentOptionController.spec.js
@@ -23,16 +23,58 @@ class Response {
   }
 }
 
-describe('/api/paymentoption/:id', () => {
-  describe('/?(id = 1)', () => {
+describe('/api/paymentoption/:userid', () => {
+  describe('/?(user_id = 1)', () => {
     it('Should return all payment options for user ID = 1', done => {
-      const id = 1;
+      const user_id = 1;
       const req = new Request();
       const res = new Response();
-      req.params.id = id;
+      req.params.id = user_id;
 
       controller.findAllPaymentOptionByUser(req, res).then(response => {
         expect(response.body.length).toBe(2);
+        done();
+      });
+    });
+  });
+
+  describe('/?(user_id = 2)', () => {
+    it('Should return all payment options for user ID = 2', done => {
+      const user_id = 2;
+      const req = new Request();
+      const res = new Response();
+      req.params.id = user_id;
+
+      controller.findAllPaymentOptionByUser(req, res).then(response => {
+        expect(response.body.length).toBe(1);
+        done();
+      });
+    });
+  });
+
+  describe('/?(user_id = 3)', () => {
+    it('Should return all payment options for user ID = 3', done => {
+      const user_id = 3;
+      const req = new Request();
+      const res = new Response();
+      req.params.id = user_id;
+
+      controller.findAllPaymentOptionByUser(req, res).then(response => {
+        expect(response.body.length).toBe(1);
+        done();
+      });
+    });
+  });
+
+  describe('/?(user_id = 4)', () => {
+    it('Should return error message', done => {
+      const user_id = 4;
+      const req = new Request();
+      const res = new Response();
+      req.params.id = user_id;
+
+      controller.findAllPaymentOptionByUser(req, res).then(response => {
+        expect(response.body).toBe('ID passed is invalid');
         done();
       });
     });
