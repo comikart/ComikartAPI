9e2f442cef770599f5f3c9e047c2063f14d23a7c
ktan114 <ktaneight@gmail.com>
2019-05-29 17:27:06 -0400

    Add the paymentOptionById route

--- api/controllers/paymentOptionController.js
+++ api/controllers/paymentOptionController.js
@@ -1,7 +1,7 @@
 const service = require('../services/paymentOptionService');
 
 /**
- * @api {get} /api/paymentoption/:id Retrieve All Payment Options By ID
+ * @api {get} /api/paymentoption/:userid Retrieve All Payment Options By ID
  * @apiVersion 1.0.0
  * @apiName GETPaymentOptions
  * @apiGroup PaymentOptions
@@ -41,6 +41,37 @@ const findAllPaymentOptionByUser = (req, res) => {
     .catch(err => res.status(400).json(err));
 };
 
+/**
+ * @api {get} /api/paymentoption/:id Retrieve All Payment Options By ID
+ * @apiVersion 1.0.0
+ * @apiName GETPaymentOptions
+ * @apiGroup PaymentOptions
+ *
+ * @apiSuccess {object[]} List all payment options
+ * @apiParam {Param} ID is user ID, used to find all payment options
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 OK
+ *    {
+ *      "credit_card": 424242424242,
+ *      "billing_address": "123 whambam st",
+ *      "exp": "05/20",
+ *      "security_number": 444,
+ *      "active": false,
+ *      "user_id": 1,
+ *      "type_id": 1,
+ *    },
+ */
+
+const findAllPaymentOptionById = (req, res) => {
+  const { id } = req.params;
+
+  return service
+    .findAllPaymentOptionById(id)
+    .then(paymentOptions => res.json(paymentOptions))
+    .catch(err => res.status(400).json(err));
+};
+
 module.exports = {
   findAllPaymentOptionByUser,
+  findAllPaymentOptionById,
 };
