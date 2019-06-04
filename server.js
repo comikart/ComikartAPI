d0194b35a95124dd786509f3b5ea6b995a86ac80
ktan114 <ktaneight@gmail.com>
2019-06-03 23:16:46 -0400

    Separated the productController

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const server = express();
 const userController = require('./api/controllers/userController');
 const paymentOptionController = require('./api/controllers/paymentOptionController');
 const purchaseController = require('./api/controllers/purchaseController');
+const productController = require('./api/controllers/productController');
 
 // mount middleware
 server.use(cors());
@@ -34,6 +35,7 @@ server.use(express.json());
 // route handler.
 server.use('/api/user', userController);
 server.use('/api/user/:id/paymentoption', paymentOptionController);
-server.uses('/api/user/:id/purchase', purchaseController);
+server.use('/api/user/:id/purchase', purchaseController);
+server.use('/api/product', productController);
 
 module.exports = server;
