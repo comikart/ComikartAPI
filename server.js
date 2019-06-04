0d1c775d87ccf957fa4d6192c26d38556320fc44
ktan114 <ktaneight@gmail.com>
2019-06-04 00:04:53 -0400

    Refactored the coupon controller #20

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const paymentOptionController = require('./api/controllers/paymentOptionControll
 const purchaseController = require('./api/controllers/purchaseController');
 const productController = require('./api/controllers/productController');
 const reviewController = require('./api/controllers/reviewController');
+const couponController = require('./api/controllers/couponController');
 
 // mount middleware
 server.use(cors());
@@ -38,6 +39,7 @@ server.use('/api/user', userController);
 server.use('/api/user/:id/paymentoption', paymentOptionController);
 server.use('/api/user/:id/purchase', purchaseController);
 server.use('/api/product', productController);
+server.use('/api/product', couponController);
 server.use('/api/product/:product_id/review', reviewController);
 
 module.exports = server;
