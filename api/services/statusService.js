0910339407afea8bd4fc5d50beffc77d6a676d5e
ktan114 <ktaneight@gmail.com>
2019-06-06 23:29:23 -0400

    Removed unnecessary service functions

--- api/services/statusService.js
+++ api/services/statusService.js
@@ -9,38 +9,14 @@ const findStatusById = id =>
     .where({ id })
     .first();
 
+// GET by title
 const findStatusByTitle = title =>
   knex('status')
     .where({ title })
     .first();
 
-// POST
-const saveStatus = status => knex('status').insert(status);
-
-// PUT
-const updateStatus = (id, status) =>
-  knex('status')
-    .where({ id })
-    .update(status);
-
-// DELETE
-const deleteStatus = status =>
-  knex('status')
-    .where({ id: status.id })
-    .del();
-
-// DELETE by ID
-const deleteStatusById = id =>
-  knex('status')
-    .where({ id })
-    .del();
-
 module.exports = {
   findAllStatus,
   findStatusById,
   findStatusByTitle,
-  saveStatus,
-  updateStatus,
-  deleteStatus,
-  deleteStatusById
 };
