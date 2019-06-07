885f01484aed7a7ba45726f833f9f64d2d49e4fc
ktan114 <ktaneight@gmail.com>
2019-06-07 14:28:34 -0400

    Completed findAllCoupons, findCouponById tests #97

---
+++ tests/unit/couponService.spec.js
@@ -0,0 +1,85 @@
+const mockDb = require('mock-knex');
+
+const knex = require('../../db/knex');
+const service = require('../../api/services/couponService');
+const development = require('../../knexfile').development;
+
+const tracker = mockDb.getTracker();
+const db = knex(development);
+
+beforeAll(() => {
+  mockDb.mock(db);
+});
+
+beforeEach(() => {
+  tracker.install();
+});
+
+afterEach(() => {
+  tracker.uninstall();
+});
+
+afterAll(() => {
+  mockDb.unmock(db);
+});
+
+describe('Test couponService', () => {
+  describe('Test findAllCoupons', () => {
+    it('Should return all coupons', done => {
+      const seed = [
+        { code: 'Hello,World', discount: 14.5, is_percent: true },
+        { code: 'MAY_2019', discount: 49.5, is_percent: false },
+        { code: 'COMIKART_2019', discount: 49.5, is_percent: true },
+      ];
+
+      tracker.on('query', query => {
+        const regex = /select\s\*\sfrom\s"coupon"/;
+        expect(regex.test(query.sql)).toBe(true);
+        expect(query.method).toBe('select');
+        query.response(seed);
+      });
+
+      return service.findAllCoupons().then(res => {
+        expect(res).toEqual(seed);
+        done();
+      });
+    });
+  });
+
+  describe('Test findCouponById', () => {
+    it('Should return a coupon by ID', done => {
+      const coupon = { code: 'MAY_2019', discount: 49.5, is_percent: false };
+
+      tracker.on('query', query => {
+        const regex = /select\s\*\sfrom\s"coupon"\swhere\s"id"\s\=\s\$1/;
+        expect(regex.test(query.sql)).toBe(true);
+        expect(query.method).toBe('select');
+        expect(query.bindings).toEqual([2]);
+        query.response(coupon);
+      });
+
+      return service.findCouponById(2).then(res => {
+        expect(res).toBe(coupon);
+        done();
+      });
+    });
+    it('Should return a coupon by ID', done => {
+      const coupon = { code: 'Hello,World', discount: 14.5, is_percent: true };
+
+      tracker.on('query', query => {
+        const regex = /select\s\*\sfrom\s"coupon"\swhere\s"id"\s\=\s\$1/;
+        expect(regex.test(query.sql)).toBe(true);
+        expect(query.method).toBe('select');
+        expect(query.bindings).toEqual([1]);
+        query.response(coupon);
+      });
+
+      return service.findCouponById(1).then(res => {
+        expect(res).toBe(coupon);
+        done();
+      });
+    });
+  });
+
+ 
+});
