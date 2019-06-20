05d4ee06e79b6af60cfdb6b0506303859b27412b
Eric <eric_gomez_1@hotmail.com>
2019-06-20 02:06:03 -0700

    finished with a majority of admin routes and services. #131 #130

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { authorization } = require('../utils/security');
+const adminService = require('../services/adminService.js');
 
 router.use('/', authorization);
 
-// purchase history filter by date,
+// purchase history filter by date, and status.
 // shipping history filter by date.
 // clients list filter by id
 // products list
@@ -11,8 +12,66 @@ router.use('/', authorization);
 // reviews by product
 // comments by review
 
+router.route('/clients').get((req, res) => {
+  return adminService.findAllClients().then(clients => res.json(clients));
+});
+
+router.route('/purchases').get((req, res) => {
+  const { status } = req.query;
+  return adminService
+    .findPurchases(status)
+    .then(purchases => res.json(purchases));
+});
+
+router.route('/purchases/:id').put((req, res) => {
+  const { id } = req.params;
+  const { status_id } = req.body;
+  adminService.updatePurchaseStatus(id, status_id).then(() => res.json({}));
+});
+
+router.route('/total-purchases').get((req, res) => {
+  return adminService
+    .findTotalPurchasesByMonthAndYear()
+    .then(history => res.json(history))
+    .catch(err => res.status(400).json(err));
+});
+
+router.route('/sales').get((req, res) => {
+  const { start, end } = req.query;
+  return adminService
+    .findTotalSalesByMonthAndYear(start, end)
+    .then(sales => res.json(sales))
+    .catch(err => res.status(400).json(err));
+});
+
 router
-  .route('/')
-  .get((req, res) => res.json({ message: 'successfully retrieved data.' }));
+  .route('/products')
+  .get((req, res) => {
+    return adminService.findAllProducts().then(products => res.json(products));
+  })
+  .post((req, res) => {
+    const product = req.body;
+    adminService.saveProduct(product).then(() => res.status(201).json({}));
+  });
+
+router
+  .route('/products/:product_id')
+  .get((req, res) => {
+    const { product_id } = req.params;
+    return adminService
+      .findProductById(product_id)
+      .then(product => res.json(product));
+  })
+  .put((req, res) => {
+    const { product_id } = req.params;
+    const update = req.body;
+    return adminService.updateProduct(product_id).then(() => res.json({}));
+  })
+  .delete((req, res) => {
+    const { product_id } = req.params;
+    return adminService
+      .deleteProduct(product_id)
+      .then(() => res.status(204).json({}));
+  });
 
 module.exports = router;
--- api/services/adminService.js
+++ api/services/adminService.js
@@ -1,3 +1,83 @@
-const knext = require('../../db/knex');
+const knex = require('../../db/knex');
 
-const findAllClients = () => knext('user').where({ role_id: 2 });
+const findAllClients = () => knex('user').where({ role_id: 2 });
+
+const findAllProducts = () => knex('product').select();
+
+const findProductById = id => knex('product').where({ id });
+
+const updateProduct = (id, product) =>
+  knex('product')
+    .where({ id })
+    .update(product);
+
+const deleteProduct = id =>
+  knex('product')
+    .where({ id })
+    .del();
+
+const saveProduct = product => knex('product').insert(product);
+
+/**
+ * @param {String} status Optional - filters orders by status. (open, shipping, complete, closed)
+ */
+const findPurchases = status => {
+  return knex
+    .select('status.title as status', 'purchase.*')
+    .from('purchase')
+    .innerJoin('status', 'purchase.status_id', 'status.id')
+    .where(knex.raw(`status.title like '%${status ? status : ''}%'`))
+    .orderBy('date_created', 'desc');
+};
+
+const updatePurchaseStatus = (id, status_id) => {
+  return knex('purchase')
+    .where({ id })
+    .update({ status_id });
+};
+
+const findTotalPurchasesByMonthAndYear = () => {
+  return knex
+    .select(knex.raw("to_char(purchase.date_created, 'Mon-YYYY') as date"))
+    .count('*')
+    .from('purchase')
+    .innerJoin('invoice', 'purchase.invoice_id', 'invoice.id')
+    .groupBy('date');
+};
+/**
+ * @description returns total sales in USD by month and year.
+ * @param {Date String} date_start
+ * @example 06-2018 year is optional unless adding an end date, month is also optional if searching by year
+ * @param {Date String} date_end
+ * @example 07-2019 Year is required
+ */
+const findTotalSalesByMonthAndYear = (date_start, date_end) => {
+  return knex
+    .select(knex.raw("to_char(purchase.date_created, 'Mon-YYYY') as date"))
+    .sum('total')
+    .from('purchase')
+    .innerJoin('invoice', 'purchase.invoice_id', 'invoice.id')
+    .groupBy('date')
+    .where(
+      knex.raw(
+        `to_char(purchase.date_created, 'MM-YYYY') ${
+          date_end
+            ? `between '%${date_start}%' and '%${date_end}%'`
+            : `like '%${date_start ? date_start : ''}%'`
+        }`,
+      ),
+    );
+};
+
+module.exports = {
+  findAllClients,
+  findAllProducts,
+  findProductById,
+  updateProduct,
+  saveProduct,
+  deleteProduct,
+  findPurchases,
+  updatePurchaseStatus,
+  findTotalPurchasesByMonthAndYear,
+  findTotalSalesByMonthAndYear,
+};
