1394949476b751a9b7ef7f5b6213a981861890c8
Eric <eric_gomez_1@hotmail.com>
2019-07-08 13:55:53 -0700

    updated the actions column

--- pages/admin/products.jsx
+++ pages/admin/products.jsx
@@ -80,20 +80,23 @@ class Products extends Component {
                 <td>$ {product.unit_price}</td>
                 <td>{`${this.props.products[0].is_discontinued}`}</td>
                 <td className='text-center'>
-                  <ButtonGroup>
-                    <Button
-                      onClick={() => this.handleUpdate(product)}
-                      className='btn btn-link mr-2'
-                    >
-                      Edit
-                    </Button>
-                    <Button
-                      onClick={() => this.props.deleteProduct(product.id)}
-                      className='btn btn-link'
-                    >
-                      Delete
-                    </Button>
-                  </ButtonGroup>
+                  <Button
+                    className='btn-icon btn-round'
+                    color='success'
+                    size='sm'
+                    onClick={() => this.handleUpdate(product)}
+                  >
+                    <i className='tim-icons icon-pencil' />
+                  </Button>
+                  {` `}
+                  <Button
+                    className='btn-icon btn-round'
+                    color='danger'
+                    size='sm'
+                    onClick={() => this.props.deleteProduct(product.id)}
+                  >
+                    <i className='tim-icons icon-simple-remove' />
+                  </Button>
                 </td>
               </tr>
             ))}
